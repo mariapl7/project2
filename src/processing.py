fa2e22ea87b3aaba9da1eabed8615a8c98f74706
miplo <miplotnova@mail.ru>
2024-08-28 22:40:22 +0500

    init commit

--- src/processing.py
+++ src/processing.py
@@ -3,7 +3,8 @@ inform_state = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:
              {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
              {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 def filter_by_state(inform_state: list[str], state_id='EXECUTED'):
-    """Функция возвращает новый список словарей"""
+    """Функция возвращает новый список словарей содержащий только те словари, у которых ключ
+state"""
     filter_state = []
     for key in inform_state:
         if key.get('state') == state_id:
@@ -13,7 +14,7 @@ def filter_by_state(inform_state: list[str], state_id='EXECUTED'):
 print(filter_by_state(inform_state))
 
 def sort_by_date(inform_state: list[str], reverse=True):
-    """Функция должна возвращать новый список, отсортированный по дате"""
+    """Функция возвращает новый список, отсортированный по дате"""
     sorted_inform_state = sorted(inform_state, key=lambda inform_state: inform_state['date'], reverse=True)
     return sorted_inform_state
 
