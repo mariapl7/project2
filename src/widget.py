331f3382d98e8d05f11b2303477fb52554a349fa
miplo <miplotnova@mail.ru>
2024-09-01 18:35:41 +0500

    homework 10_1 done

--- src/widget.py
+++ src/widget.py
@@ -1,19 +1,19 @@
-def mask_account_card(card_number: str) -> list[str]:
-    """Функция для маскировки карт и счетов"""
-    if card_number == 16:
-        mask_number = card_number.split()[-1]
-        name_card = " ".join(card_number)
-        mask_name_number = name_card + mask_number
-        return mask_name_number
-    elif card_number == 20:
-        mask_number = mask_account_card(card_number.split()[-1])
-        name_card = " ".join(card_number)
-        mask_name_number = name_card + mask_number
-        return mask_name_number
-
-print(mask_account_card('Счет 73654108430135874305'))
-print(mask_account_card('Maestro 7000792289606361'))
-print(mask_account_card('Visa Platinum 7000792289606361'))
+# def mask_account_card(card_number: str) -> list[str]:
+#     """Функция для маскировки карт и счетов"""
+#     if card_number == 16:
+#         mask_number = card_number.split()[-1]
+#         name_card = " ".join(card_number)
+#         mask_name_number = name_card + mask_number
+#         return mask_name_number
+#     elif card_number == 20:
+#         mask_number = mask_account_card(card_number.split()[-1])
+#         name_card = " ".join(card_number)
+#         mask_name_number = name_card + mask_number
+#         return mask_name_number
+#
+# print(mask_account_card('Счет 73654108430135874305'))
+# print(mask_account_card('Maestro 7000792289606361'))
+# print(mask_account_card('Visa Platinum 7000792289606361'))
 
 
 def get_date(data: str) -> str:
