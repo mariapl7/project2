9eb8624400d41124c125ba34c4a30e7d56ef1682
miplo <miplotnova@mail.ru>
2024-09-03 22:34:19 +0500

    homework_10_2

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,13 +1,11 @@
-import pytest
 from src.widget import get_date
 
 
-@pytest.fixture()
-def data():
-    return ["2024-03-11T02:26:18.671407"]
-
-
-@pytest.mark.parametrize("data, expected", ["2024-03-11T02:26:18.671407"])
-def test_get_date(data: str, expected):
+@pytest.mark.parametrize("data, expected", [("2024-03-11T02:26:18.671407", "11.03.2024"),
+                                            ("", "Неверно задана дата"),
+                                            ("2024-03-11", "11.03.2024"),
+                                            ("2024-030211554478411", "Неверно задана дата")
+                                            ])
+def test_get_date(data: str, expected) -> None:
     """Функция тестирования для функции get_date"""
     assert test_get_date(data) == expected
