a59dcab7872e5f8c8ac9ee9556367d9b08262cc5
miplo <miplotnova@mail.ru>
2024-09-03 22:33:36 +0500

    homework_10_2

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,31 +1,16 @@
-import pytest
-from src.masks import get_mask_card_number
-
-
-@pytest.fixture
-def card_number():
-    return ["7000792289606361", "7000 79** **** 6361", " ", "700079228968"]
+from src.masks import get_mask_card_number, get_mask_account
 
 
 @pytest.mark.parametrize("card_number, expected", [("7000792289606361", "7000 79** **** 6361"),
-                                                   ("", None),
+                                                   (" ", None),
                                                    ("700079228968", None),
                                                    ])
-def test_get_mask_card_number(card_number: str, expected):
+def test_get_mask_card_number(card_number: str, expected) -> None:
     """Функция тестирования для функции get_mask_card_number"""
     assert get_mask_card_number(card_number) == expected
 
 
-import pytest
-from src.masks import get_mask_account
-
-
-@pytest.fixture
-def bank_account():
-    return [606361]
-
-
 @pytest.mark.parametrize("bank_account, expected", [606361])
-def test_get_mask_account(bank_account: str, expected):
+def test_get_mask_account(bank_account: str, expected) -> None:
     """Функция тестирования для функции get_mask_account"""
     assert get_mask_account(bank_account) == expected
