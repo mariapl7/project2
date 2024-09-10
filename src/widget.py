7cfdc81bf0a30f5b2bcd9ccfa52c445be91087e8
miplo <miplotnova@mail.ru>
2024-09-11 00:32:24 +0500

    homework_10_2

--- src/widget.py
+++ src/widget.py
@@ -1,23 +1,28 @@
-# def mask_account_card(card_number: str) -> list[str]:
-#     """Функция для маскировки карт и счетов"""
-#     if card_number == 16:
-#         mask_number = card_number.split()[-1]
-#         name_card = " ".join(card_number)
-#         mask_name_number = name_card + mask_number
-#         return mask_name_number
-#     elif card_number == 20:
-#         mask_number = mask_account_card(card_number.split()[-1])
-#         name_card = " ".join(card_number)
-#         mask_name_number = name_card + mask_number
-#         return mask_name_number
-#
-# print(mask_account_card('Счет 73654108430135874305'))
-# print(mask_account_card('Maestro 7000792289606361'))
-# print(mask_account_card('Visa Platinum 7000792289606361'))
+from typing import Any
+from src.masks import get_mask_card_number, get_mask_account
 
 
-def get_date(data: str) -> str:
-    """Функция преобразования даты формата "2018-07-11T02:26:18.671407" в формат "11.07.2018"""
-    return f'{data[8:10]}.{data[5:7]}.{data[0:4]}'
+def mask_account_card(card_number: str) -> Any:
+    """Функция для маскировки карт и счетов"""
+    card_number_mask = card_number.split()
+    if len(card_number_mask) > 1 and len(card_number_mask[-1]) == 16:
+        mask_number = get_mask_card_number(card_number_mask[-1])
+        if mask_number is not None:
+            card_number_mask[-1] = mask_number
+            return " ".join(card_number_mask)
+    elif len(card_number_mask) > 1 and len(card_number_mask[-1]) == 20:
+        mask_number = get_mask_account(card_number_mask[-1])
+        if mask_number is not None:
+            card_number_mask[-1] = mask_number
+            return " ".join(card_number_mask)
+    else:
+        return "Ошибка ввода данных"
 
-print(get_date("2024-03-11T02:26:18.671407"))
+
+def get_date(user_data: str) -> str:
+    """Функция преобразования даты формата в формат "ДД.ММ.ГГГГ"""
+    date_for_format = user_data[0:10].split("-")
+    if len(user_data) >= 10 and len(date_for_format) == 3:
+        return ".".join(date_for_format[::-1])
+    else:
+        return "Неверно задана дата"
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -21,9 +21,9 @@ from src.processing import filter_by_state, sort_by_date
         ),
     ]
 )
-def test_filter_by_state(info_list: list[str], state: str, expected) -> str:
+def test_filter_by_state(list_info, state, expected):
     """Функция тестирования для функции filter_by_state"""
-    assert filter_by_state(state) == expected
+    assert filter_by_state(list_info, state) == expected
 
 
 @pytest.mark.parametrize(
@@ -32,23 +32,23 @@ def test_filter_by_state(info_list: list[str], state: str, expected) -> str:
         (
             True,
             [
-                {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-                {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
             ],
         ),
         (
             False,
             [
-                {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-                {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-                {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
-            ]
+                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            ],
         ),
     ]
 )
-def test_sort_by_date(date_list: list[str], sort_reverse, expected) -> str:
+def test_sort_by_date(list_info, sort_reverse, expected):
     """Функция тестирование функции sort_by_date"""
-    assert sort_by_date(date_list, sort_reverse) == expected
+    assert sort_by_date(list_info, sort_reverse) == expected
