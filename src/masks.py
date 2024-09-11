7770ed5163f3d0886d219521105b234d97556fb6
miplo <miplotnova@mail.ru>
2024-09-11 06:52:05 +0500

    homework_10_2

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 def get_mask_card_number(card_number: str) -> str | None:
     """Функция маскировки номера банковской карты"""
     if card_number.isdigit() and len(card_number) == 16:
-        return f"{card_number[:4]} {card_number[:2]}** **** {card_number[-4:]}"
+        return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
     else:
         return None
 
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,7 +2,7 @@ import pytest
 from src.masks import get_mask_card_number, get_mask_account
 
 
-@pytest.mark.parametrize("card_number, expected", [("7000792289606361", "7000 70** **** 6361"),
+@pytest.mark.parametrize("card_number, expected", [("7000792289606361", "7000 79** **** 6361"),
                                                    (" ", None),
                                                    ("700079228968", None),
                                                    ],)
