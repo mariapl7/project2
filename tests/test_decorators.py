c5f7f3a43a33ec958555e2ea5090d8d9ee5f21e6
miplo <miplotnova@mail.ru>
2024-09-19 22:19:41 +0500

    homework_11_2

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -10,3 +10,19 @@ def test_log(capsys):
     captured = capsys.readouterr()
     assert captured.out == "my_function ok"
     assert result == 3
+
+
+def test_log_exception(filename="mylog.txt"):
+    @log(filename)
+    def my_function(x, y):
+        return x / y
+
+    my_function(1, 2)
+    with open(filename, "r") as file:
+        message = file.read()
+    assert message == "my_function error: Inputs: (1, 0), {}\n"
+
+
+# def test_log_exception():
+#     with pytest.raises(Exception, match="my_function error: Inputs: (1, 2), {}"):
+#         my_function()
