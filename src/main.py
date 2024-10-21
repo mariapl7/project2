0f8ada35a34ee6d4353b4d33075cb92eea772886
miplo <miplotnova@mail.ru>
2024-10-21 20:32:18 +0500

    homework_13_2

--- src/main.py
+++ src/main.py
@@ -41,77 +41,77 @@ def main():
         if status != "EXECUTED" and status != "CANCELED" and status != "PENDING":
             print(f"Статус операции {status} недоступен.")
             continue
-        print(f"Операции отфильтрованы по статусу {status}")
-        filtered_transactions = filter_by_state(transactions_file, status)
-        break
+        else:
+            break
 
-        print("Отсортировать операции по дате? Да/Нет")
-        sort_choice = input("Введите да или нет: ").lower()
+    print(f"Операции отфильтрованы по статусу {status}")
+    filtered_transactions = filter_by_state(transactions_file, status)
 
-        if sort_choice == 'да':
-            print("Отсортировать по возрастанию или по убыванию?")
-            sort_order = input("Отсортировать по возрастанию или по убыванию? ").lower()
-            if sort_order == "в порядке убывания":
-                reverse = True
-                filtered_transactions = sort_by_date(transactions_file, reverse)
-            elif sort_order == "в порядке возрастания":
-                reverse = False
-                filtered_transactions = sort_by_date(transactions_file, reverse)
+    print("Отсортировать операции по дате? Да/Нет")
+    sort_choice = input("Введите да или нет: ").lower()
+
+    if sort_choice == 'да':
+        print("Отсортировать по возрастанию или по убыванию?")
+        sort_order = input("Отсортировать по возрастанию или по убыванию? ").lower()
+        if sort_order == "в порядке убывания":
+            reverse = True
+            filtered_transactions = sort_by_date(transactions_file, reverse)
+        elif sort_order == "в порядке возрастания":
+            reverse = False
+            filtered_transactions = sort_by_date(transactions_file, reverse)
+        else:
+            print("Введен некорректный ответ.")
+            return
+    else:
+        print("Введен некорректный ответ.")
+        return
+
+    print("Выводить только рублевые транзакции? Да/Нет")
+    currency_filter = input("Введите да или нет: ").lower()
+    if currency_filter == "да":
+        rub_trans = []
+        for trans in filtered_transactions:
+            if currency_filter == "1" or currency_filter == "2":
+                if trans["operationAmount"]["currency"]["code"] == "RUB":
+                    rub_trans.append(trans)
             else:
-                print("Введен некорректный ответ.")
-                return
-        elif sort_order == "нет":
-            filtered_transactions = filter_state
+                if trans["currency_code"] == "RUB":
+                    rub_trans.append(trans)
+    elif currency_filter == "нет":
+        rub_trans = []
+        for trans in filtered_transactions:
+            rub_trans.append(trans)
         else:
             print("Введен некорректный ответ.")
             return
 
-        print("Выводить только рублевые транзакции? Да/Нет")
-        currency_filter = input("Введите да или нет: ").lower()
-        if currency_filter == "да":
-            rub_trans = []
-            for trans in filtered_transactions:
-                if currency_filter == "1" or currency_filter == "2":
-                    if trans["operationAmount"]["currency"]["code"] == "RUB":
-                        rub_trans.append(trans)
-                else:
-                    if trans["currency_code"] == "RUB":
-                        rub_trans.append(trans)
-        elif currency_filter == "нет":
-            rub_trans = []
-            for trans in filtered_transactions:
-                rub_trans.append(trans)
-        else:
-            print("Введен некорректный ответ.")
-            return
-
-        print("Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
-        filter_by_word = input("Введите да или нет: ").lower()
-        if filter_by_word == "да":
-            filter_by_word_yes = input("Введите слово для фильтрации: ")
-            trans_word = []
-            for trans in rub_trans:
-                if filter_by_word_yes in trans["description"]:
-                    trans_word.append(trans)
-        elif filter_by_word == "нет":
-            trans_word = []
-            for trans in rub_trans:
+    print("Отфильтровать список транзакций по определенному слову в описании? Да/Нет")
+    filter_by_word = input("Введите да или нет: ").lower()
+    if filter_by_word == "да":
+        filter_by_word_yes = input("Введите слово для фильтрации: ")
+        trans_word = []
+        for trans in filtered_transactions:
+            if filter_by_word_yes in trans["description"]:
                 trans_word.append(trans)
-        else:
-            print("Введен некорректный ответ.")
-            return
-        if (len(trans_word)) == 0:
-            print("Не найдено ни одной транзакции, подходящей под ваши условия фильтрации")
-            return
+    elif filter_by_word == "нет":
+        trans_word = []
+        for trans in filtered_transactions:
+            trans_word.append(trans)
+    else:
+        print("Введен некорректный ответ.")
+        return
+    if (len(trans_word)) == 0:
+        print("Не найдено ни одной транзакции, подходящей под ваши условия фильтрации")
+        return
 
-        print("Распечатываю итоговый список транзакций...")
-        print(f"Всего банковских операций в выборке: {len(trans_word)}\n")
+    print("Распечатываю итоговый список транзакций...")
+    print(f"Всего банковских операций в выборке: {len(trans_word)}\n")
 
-        for transaction in filtered_transactions:
-            print(f"{transaction['date']} {transaction['description']}")
-            print(f"Счет: {transaction['account']}")
-            print(f"Сумма: {transaction['amount']} {transaction['currency']}")
-            print()
+    for transaction in filtered_transactions:
+        print(f"{transaction['date']} {transaction['description']}")
+        print(f"Счет: {transaction['account']}")
+        print(f"Сумма: {transaction['amount']} {transaction['currency']}")
+        print()
 
 
 if __name__ == "__main__":
